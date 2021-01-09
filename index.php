fe820bb093a2965561f5551714c0b4441bd30fc1
Nick <ngramste@gmail.com>
2021-01-09 14:46:56 -0600

    Add Calendar
    Updated to include calendar interface for viewing old images

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="./css/w3.css">
     <link rel="stylesheet" href="./css/w3-theme-dark-grey.css">
+    <link rel="stylesheet" href="./css/main.css">
     <script src="main.js"></script>
   </head>
   <body class="w3-theme-d4">
@@ -14,7 +15,7 @@
 
       <h1 class="w3-left-align w3-monospace w3-text-orange">Weather Station: <?php echo $config->stationName; ?></h1>
       <h2 class="w3-left-align w3-monospace" id="location"><?php echo $config->location->lat . ", " . $config->location->lon; ?></h2>
-      
+
       <?php
         $files = preg_grep('/^([^.])/', scandir("./audio", SCANDIR_SORT_DESCENDING));
 
@@ -23,7 +24,7 @@
           array_push($renamed, explode("-", $files[$i])[1] . explode("-", $files[$i])[2] . "_" . $files[$i]);
         }
         rsort($renamed);
-        
+
         $files = [];
         for ($i = 0; $i < sizeof($renamed); $i++) {
           array_push($files, explode("_", $renamed[$i])[1]);
@@ -48,42 +49,63 @@
         <button class="w3-bar-item w3-button tablink" onclick="openImg(event,'NO')">NO</button>
         <button class="w3-bar-item w3-button tablink" onclick="openImg(event,'THERM')">THERM</button>
         <button class="w3-bar-item w3-button tablink" onclick="openImg(event,'ZA')">ZA</button>
-        <button class="w3-bar-item w3-button tablink" onclick="openImg(event,'Archive')">Archive</button>
+        <button class="w3-bar-item w3-button tablink" onclick='updateCalendar(event, 0, 0, <?php echo json_encode($files); ?>)'>Archive</button>
+        <button class="w3-bar-item w3-button tablink"><a href="/" style="text-decoration: none;">Latest</a></button>
       </div>
 
-      
       <div id="MCIR" class="map w3-container w3-border w3-border-black">
         <img class="w3-image" style="width:100%" src="./images/<?php echo $fileBaseName; ?>-MCIR.png">
       </div>
-    
+
       <div id="MSA" class="map w3-container w3-border w3-border-black" style="display:none">
         <img class="w3-image" style="width:100%" src="./images/<?php echo $fileBaseName; ?>-MSA.png">
       </div>
-    
+
       <div id="NO" class="map w3-container w3-border w3-border-black" style="display:none">
         <img class="w3-image" style="width:100%" src="./images/<?php echo $fileBaseName; ?>-NO.png">
       </div>
-    
+
       <div id="THERM" class="map w3-container w3-border w3-border-black" style="display:none">
         <img class="w3-image" style="width:100%" src="./images/<?php echo $fileBaseName; ?>-THERM.png">
       </div>
-    
+
       <div id="ZA" class="map w3-container w3-border w3-border-black" style="display:none">
         <img class="w3-image" style="width:100%" src="./images/<?php echo $fileBaseName; ?>-ZA.png">
       </div>
-      
-      <div id="Archive" class="map w3-container w3-border w3-border-black w3-theme" style="display:none;">
-      <ul class="w3-ul w3-hoverable w3-xlarge">
-          <?php
-            echo "<a href='/'><li>Latest</li></a>";
-            for ($i = 0; $i < sizeof($files); $i++) {
-              $sat = explode("-", $files[$i])[0];
-              $timestamp = date ("F d, Y H:i:s", filemtime("./audio/" . $files[$i]));
 
-              echo "<a href='?file=$files[$i]'><li>$sat: $timestamp</li></a>";
+      <div id="Archive" class="map w3-border w3-border-black w3-theme" style="display:none;">
+        <div id="calendar">
+
+        </div>
+
+        <div id="archiveListing">
+          <?php
+            $currentID = "";
+            for ($i = 0; $i < sizeof($files); $i++) {
+              $newID = substr($files[$i], 7, 8);
+              if ($newID != $currentID) {
+                if ($currentID != "") {
+                  echo "</ul>";
+                  echo "</div>";
+                }
+                $currentID = $newID;
+                echo "<div id=\"$newID\" class=\"imageList w3-container w3-border w3-border-black\" style=\"display:none\">";
+                echo "<ul>";
+
+                $sat = explode("-", $files[$i])[0];
+                $timestamp = date ("F d, Y H:i:s", filemtime("./audio/" . $files[$i]));
+
+                echo "<a href='?file=$files[$i]'><li>$sat: $timestamp</li></a>";
+
+              } else {
+                echo "<a href='?file=$files[$i]'><li>$sat: $timestamp</li></a>";
+              }
             }
+            echo "</ul>";
+            echo "</div>";
           ?>
-        </ul>
+        </div>
+
       </div>
 
     </div>
--- main.js
+++ main.js
@@ -11,3 +11,96 @@ function openImg(evt, image) {
   document.getElementById(image).style.display = "block";
   evt.currentTarget.className += " w3-orange";
 }
+
+function listImages(evt, id) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("imageList");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(id).style.display = "block";
+}
+
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+function updateCalendar(event, year, month, files) {
+  openImg(event, 'Archive');
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
+  if (0 == month && 0 == year) {
+    var d = new Date();
+    month = d.getMonth();
+    year = d.getFullYear();
+  }
+
+  if (month >= 12) {
+    month = 0;
+    year++;
+  }
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+
+  var firstDay = new Date(year, month, 1).getDay();
+  var numDays = new Date(year, month + 1, 0).getDate();
+
+  var html =
+      "<div class=\"month w3-orange\">"+
+      "<ul>"+
+      "  <li class=\"prev w3-button\" onclick='updateCalendar(event,"+year+","+(month - 1)+","+JSON.stringify(files)+")'>&#10094;</li>"+
+      "  <li class=\"next w3-button\" onclick='updateCalendar(event,"+year+","+(month + 1)+","+JSON.stringify(files)+")'>&#10095;</li>"+
+      "  <li>" + monthNames[month] + "<br>"+
+      "    <span style=\"w3-xxlarge\">" + year + "</span>"+
+      "  </li>"+
+      "</ul>"+
+      "</div>"+
+      ""+
+      "<ul class=\"weekdays w3-theme-d1\">"+
+      "<li>Su</li>"+
+      "<li>Mo</li>"+
+      "<li>Tu</li>"+
+      "<li>We</li>"+
+      "<li>Th</li>"+
+      "<li>Fr</li>"+
+      "<li>Sa</li>"+
+      "</ul>"+
+      ""+
+      "<ul class=\"days w3-theme-l1\">";
+
+  var days = new Array(numDays);
+  for (var i = 0; i < numDays; i++) {
+    days[i] = new Array();
+  }
+
+  for (var i = 0; i < files.length; i++) {
+    imgYear = Number(files[i].substring(7, 11));
+    imgMonth = Number(files[i].substring(11, 13)) - 1;
+    imgDay = Number(files[i].substring(13, 15));
+
+    if (imgYear == year && imgMonth == month) {
+      days[imgDay - 1].push(files[i]);
+    }
+  }
+
+  for (var i = 0; i < firstDay; i++) {
+    html += "<li></li>";
+  }
+
+  for (var i = 1; i <= numDays; i++) {
+    if (0 < days[i-1].length) {
+      html += "<li class=\"w3-orange w3-button\" onclick=\"listImages(event, " + imgYear + "" + pad(imgMonth + 1, 2) + "" + pad(i, 2) + ")\">" + i + "</li>";
+    } else {
+      html += "<li class=\"w3-button\">" + i + "</li>";
+    }
+  }
+  html +=
+      "</ul>"+
+      "</div>";
+
+  document.getElementById("calendar").innerHTML = html;
+}
